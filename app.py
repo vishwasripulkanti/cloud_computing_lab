6b599fb3dec0578264f29b793922e839a3752b49
vishwasripulkanti <vishwasripulkanti3001@gmail.com>
2026-08-21 15:44:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ def home():
 	<h1>Hello Rgukt</h1>
 	</body>
 	</html>
-	return "flask application running successfully"
+	return """
+	<html>
+	<body>
+	<h1>hello worls</h1>
+	</body>
+	</html>
+	"""
 	
 if  __name__==('__main__'):
 	app.run('0.0.0.0',5000) #host=0.0.0.0 ; port=5000
